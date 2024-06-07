d793469276aaf9ff07edbfc42479a1fa29b8e7fb
sampilgrim-sw <sam.pilgrim@spurwing.co.uk>
2024-06-07 16:40:04 +0100

    custom cursor v1

---
+++ spurwing.js
@@ -0,0 +1,60 @@
+function customCursor() {
+	//
+	const cursor = document.querySelector(".custom-cursor"); // get cursor
+	const cursor_w = cursor.offsetWidth / 2;
+	const cursor_h = cursor.offsetHeight / 2;
+
+	let targets = gsap.utils.toArray("[data-spw-cursor='true']"); // get all targets
+
+	const xSetter = gsap.quickSetter(cursor, "x", "px");
+	const ySetter = gsap.quickSetter(cursor, "y", "px");
+
+	document.addEventListener("mousemove", mouseMove);
+
+	function mouseMove(e) {
+		xSetter(e.x - cursor_w);
+		ySetter(e.y - cursor_h);
+	}
+
+	gsap.set(cursor, {
+		scale: 0.5, // initial starting state
+	});
+
+	/* hide/show cursor on hover */
+	targets.forEach((target) => {
+		//
+		let content = target.dataset.spwCursorContent;
+
+		target.addEventListener("mouseenter", (e) => {
+			//
+			document.documentElement.classList.add("custom-cursor-on");
+
+			gsap.killTweensOf(target); // kill active tweens
+
+			console.log("play");
+
+			gsap.to(cursor, {
+				autoAlpha: 1, //show cursor
+				duration: 0.2,
+				scale: 1,
+			});
+
+			cursor.innerHTML = content; //update content
+		});
+
+		target.addEventListener("mouseleave", (e) => {
+			//
+			document.documentElement.classList.remove("custom-cursor-on");
+
+			gsap.killTweensOf(target); //kill tweens of target we're leaving to avoid cursor persisting if we move too quick
+
+			console.log("reverse");
+
+			gsap.to(cursor, {
+				autoAlpha: 0, //show cursor
+				duration: 0.2,
+				scale: 0.5,
+			});
+		});
+	});
+}
