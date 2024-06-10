2ad40bfb5d9b47f8dd931ff7f60fe49a1bfe2f0c
sampilgrim-sw <sam.pilgrim@spurwing.co.uk>
2024-06-10 17:07:29 +0100

    slider height changes

--- spurwing.js
+++ spurwing.js
@@ -1,5 +1,4 @@
 function loadSliders() {
-	//
 	/* splide defaults */
 	Splide.defaults = {
 		perMove: 1,
@@ -23,13 +22,28 @@ function loadSliders() {
 	};
 
 	/* get and launch all chosen splide instances */
-	function initializeSplide(selector, options) {
+	function initializeSplide(selector, options, initCallbacks, eventCallbacks) {
 		let targets = document.querySelectorAll(selector);
 		let splides = [];
 
 		for (let i = 0; i < targets.length; i++) {
 			let splide = new Splide(targets[i], options);
+
+			// Apply initialization callbacks if any
+			if (initCallbacks) {
+				for (let [event, callback] of Object.entries(initCallbacks)) {
+					splide.on(event, callback.bind(splide)); // Ensure 'this' is bound to the splide instance
+				}
+			}
+
 			splide.mount();
+
+			// Apply event callbacks if any
+			if (eventCallbacks) {
+				for (let [event, callback] of Object.entries(eventCallbacks)) {
+					splide.on(event, callback.bind(splide)); // Ensure 'this' is bound to the splide instance
+				}
+			}
 			splides.push(splide);
 		}
 
@@ -44,11 +58,75 @@ function loadSliders() {
 				perPage: 1,
 				autoplay: false,
 			},
+			initCallbacks: {
+				mounted: function () {
+					console.log("mounted");
+
+					this.Components.Slides.forEach((splide_slide) => {
+						const imgWrap = splide_slide.slide.querySelector(
+							".hero-slide_img-wrap"
+						);
+						const caption =
+							splide_slide.slide.querySelector(".hero-slide_footer");
+						if (imgWrap) {
+							const originalHeight = imgWrap.offsetHeight;
+							const newHeight = originalHeight * -0.05;
+							// Store the timeline in the slide element
+							splide_slide.slide.timeline = gsap
+								.timeline({ paused: true })
+								.to(imgWrap, { scaleY: 0.9, duration: 0.3, ease: "none" }, 0)
+								.to(caption, { y: newHeight, duration: 0.3, ease: "none" }, 0);
+							// play if the slide is not active
+							if (splide_slide.index !== 0) {
+								splide_slide.slide.timeline.play();
+							}
+						}
+					});
+				},
+			},
+			eventCallbacks: {
+				active: function (Slide) {
+					const newActiveSlide = Slide;
+					newActiveSlide.slide.timeline.reverse();
+				},
+				inactive: function (Slide) {
+					const newInactiveSlide = Slide;
+					newInactiveSlide.slide.timeline.play();
+				},
+				resize: function () {
+					// when window resized, recalculate timeline distances
+				},
+			},
+			helperFunctions: {
+				createTimelines: function () {
+					console.log("Create timelines");
+					this.Components.Slides.forEach((splide_slide) => {
+						const imgWrap = splide_slide.slide.querySelector(
+							".hero-slide_img-wrap"
+						);
+						const caption =
+							splide_slide.slide.querySelector(".hero-slide_footer");
+						if (imgWrap && caption) {
+							const originalHeight = imgWrap.offsetHeight;
+							const newHeight = originalHeight * -0.05;
+							// Store the timeline in the slide element
+							splide_slide.slide.timeline = gsap
+								.timeline({ paused: true })
+								.to(imgWrap, { scaleY: 0.9, duration: 0.3, ease: "none" }, 0)
+								.to(caption, { y: newHeight, duration: 0.3, ease: "none" }, 0);
+							// play if the slide is not active
+							if (splide_slide.index !== 0) {
+								splide_slide.slide.timeline.play();
+							}
+						}
+					});
+				},
+			},
 		},
 	];
 
-	sliders.forEach(({ selector, options }) => {
-		initializeSplide(selector, options);
+	sliders.forEach(({ selector, options, initCallbacks, eventCallbacks }) => {
+		initializeSplide(selector, options, initCallbacks, eventCallbacks);
 	});
 }
 
