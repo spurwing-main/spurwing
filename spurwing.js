c91eaef4f40823edcc6a4c06f5ebb8057ffdb0e5
sampilgrim-sw <sam.pilgrim@spurwing.co.uk>
2024-06-11 16:19:22 +0100

    Handle resize

--- spurwing.js
+++ spurwing.js
@@ -1,6 +1,4 @@
 function loadSliders() {
-	/* this is a change */
-
 	/* splide defaults */
 	Splide.defaults = {
 		perMove: 1,
@@ -24,34 +22,47 @@ function loadSliders() {
 	};
 
 	/* get and launch all chosen splide instances */
-	function initializeSplide(selector, options, initCallbacks, eventCallbacks) {
+	function initializeSplide(
+		selector,
+		options,
+		initCallbacks,
+		eventCallbacks,
+		helperFunctions
+	) {
 		let targets = document.querySelectorAll(selector);
 		let splides = [];
 
 		for (let i = 0; i < targets.length; i++) {
+			/* new splide instance */
 			let splide = new Splide(targets[i], options);
 
 			// Apply initialization callbacks if any
 			if (initCallbacks) {
 				for (let [event, callback] of Object.entries(initCallbacks)) {
-					splide.on(event, callback.bind(splide)); // Ensure 'this' is bound to the splide instance
+					splide.on(event, callback.bind(splide, helperFunctions)); // Ensure 'this' is bound to the splide instance
 				}
 			}
 
+			/* mount splide instance */
 			splide.mount();
+			// splide.mount(window.splide.Extensions);
 
 			// Apply event callbacks if any
 			if (eventCallbacks) {
 				for (let [event, callback] of Object.entries(eventCallbacks)) {
-					splide.on(event, callback.bind(splide)); // Ensure 'this' is bound to the splide instance
+					splide.on(event, function (...args) {
+						callback.apply(splide, [helperFunctions, ...args]);
+					});
 				}
 			}
 			splides.push(splide);
 		}
 
+		/* return all created splide instances */
 		return splides;
 	}
 
+	/* declare slider selectors, options and any callback functions */
 	const sliders = [
 		{
 			selector: ".hero-slider",
@@ -59,44 +70,31 @@ function loadSliders() {
 				type: "loop",
 				perPage: 1,
 				autoplay: false,
+				autoScroll: {
+					autoStart: false,
+				},
 			},
 			initCallbacks: {
-				mounted: function () {
+				mounted: function (helperFunctions) {
 					console.log("mounted");
-
-					this.Components.Slides.forEach((splide_slide) => {
-						const imgWrap = splide_slide.slide.querySelector(
-							".hero-slide_img-wrap"
-						);
-						const caption =
-							splide_slide.slide.querySelector(".hero-slide_footer");
-						if (imgWrap) {
-							const originalHeight = imgWrap.offsetHeight;
-							const newHeight = originalHeight * -0.05;
-							// Store the timeline in the slide element
-							splide_slide.slide.timeline = gsap
-								.timeline({ paused: true })
-								.to(imgWrap, { scaleY: 0.9, duration: 0.3, ease: "none" }, 0)
-								.to(caption, { y: newHeight, duration: 0.3, ease: "none" }, 0);
-							// play if the slide is not active
-							if (splide_slide.index !== 0) {
-								splide_slide.slide.timeline.play();
-							}
-						}
-					});
+					// Call the createTimelines helper function
+					helperFunctions.createTimelines.call(this);
 				},
 			},
 			eventCallbacks: {
-				active: function (Slide) {
-					const newActiveSlide = Slide;
-					newActiveSlide.slide.timeline.reverse();
+				active: function (helperFunctions, Slide) {
+					// grow active slide
+					// Slide is the Slide component that becomes active.
+					Slide.slide.timeline.play();
 				},
-				inactive: function (Slide) {
-					const newInactiveSlide = Slide;
-					newInactiveSlide.slide.timeline.play();
+				inactive: function (helperFunctions, Slide) {
+					// shrink inactive slide
+					// Slide is the Slide component that becomes inactive.
+					Slide.slide.timeline.reverse();
 				},
-				resize: function () {
+				resize: function (helperFunctions) {
 					// when window resized, recalculate timeline distances
+					helperFunctions.createTimelines.call(this);
 				},
 			},
 			helperFunctions: {
@@ -110,14 +108,14 @@ function loadSliders() {
 							splide_slide.slide.querySelector(".hero-slide_footer");
 						if (imgWrap && caption) {
 							const originalHeight = imgWrap.offsetHeight;
-							const newHeight = originalHeight * -0.05;
+							const newHeight = originalHeight * 0.05;
 							// Store the timeline in the slide element
 							splide_slide.slide.timeline = gsap
 								.timeline({ paused: true })
-								.to(imgWrap, { scaleY: 0.9, duration: 0.3, ease: "none" }, 0)
-								.to(caption, { y: newHeight, duration: 0.3, ease: "none" }, 0);
-							// play if the slide is not active
-							if (splide_slide.index !== 0) {
+								.to(imgWrap, { scaleY: 1.1, duration: 0.3 }, 0)
+								.to(caption, { y: newHeight, duration: 0.3 }, 0);
+							// play if the slide is  active
+							if (splide_slide.index == 0) {
 								splide_slide.slide.timeline.play();
 							}
 						}
@@ -125,11 +123,65 @@ function loadSliders() {
 				},
 			},
 		},
+		{
+			selector: '[splide="about-hero"]',
+			options: {
+				autoWidth: true,
+				focus: "center",
+				type: "loop",
+				gap: "4rem",
+				trimSpace: false,
+				lazyLoad: false,
+				drag: "free",
+				flickPower: 300,
+				flickMaxPages: 1,
+				cloneStatus: true,
+				autoScroll: {
+					speed: 0.5,
+					pauseOnHover: false,
+					pauseOnFocus: false,
+				},
+				breakpoints: {
+					991: {},
+					767: {},
+					479: {},
+				},
+			},
+			initCallbacks: {
+				mounted: function () {
+					/* on mount, add additional slides required to fill screen */
+					const splide = this;
+					const totalSlides = splide.length;
+					const requiredSlides = Math.ceil(
+						window.innerWidth / this.Components.Elements.track.clientWidth
+					);
+					if (totalSlides < requiredSlides) {
+						for (let i = totalSlides; i < requiredSlides; i++) {
+							splide.add(
+								`<div class="splide__slide">${splide.Components.Elements.slides[0].innerHTML}</div>`
+							);
+						}
+					}
+					splide.refresh();
+				},
+			},
+			eventCallbacks: {},
+			helperFunctions: {},
+		},
 	];
 
-	sliders.forEach(({ selector, options, initCallbacks, eventCallbacks }) => {
-		initializeSplide(selector, options, initCallbacks, eventCallbacks);
-	});
+	/* loop through and initialize each slider */
+	sliders.forEach(
+		({ selector, options, initCallbacks, eventCallbacks, helperFunctions }) => {
+			initializeSplide(
+				selector,
+				options,
+				initCallbacks,
+				eventCallbacks,
+				helperFunctions
+			);
+		}
+	);
 }
 
 function customCursor() {
