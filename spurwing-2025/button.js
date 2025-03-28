62dc5e747062529a5603f27220a0d47fc5d1dda6
sampilgrim-sw <sam.pilgrim@spurwing.co.uk>
2025-03-28 17:32:05 +0000

    1

--- spurwing-2025/button.js
+++ spurwing-2025/button.js
@@ -1,99 +1,164 @@
 function main() {
 	document.querySelectorAll(".button").forEach((button) => {
-		const defaultTextEl = button.querySelector(".button_text.is-default");
-		const hoverTextEl = button.querySelector(".button_text.is-hover");
+		// set params
+		const splitType = "words"; // animate words or chars
+		const el_class_out = "button-anim-out"; // class added to animated word/char
+		const el_class_in = "button-anim-in"; // class added to animated word/char
 
-		const defaultSplit = new SplitText(defaultTextEl, {
-			type: "chars",
-			charsClass: "char",
-		});
-		const hoverSplit = new SplitText(hoverTextEl, {
-			type: "chars",
-			charsClass: "char-hover",
-		});
+		const duration_out = 0.2; // duration of each
+		const duration_in = 0.2;
 
-		const fan = 3; // Adjust for more/less spread
-		const rotation = 1; // Adjust for more/less rotation
-		const center = (defaultSplit.chars.length - 1) / 2;
-		const centerHover = (hoverSplit.chars.length - 1) / 2;
-		const stagger = 0.05;
-		const staggerHover = 0.05;
-		const hoverDelay = 0.2;
-		const ease = "power2.inOut";
-		// const easeHover = "elastic.out(1, 0.5)";
-		easeHover = ease;
+		const duration_opacity_out = 0.3;
+		const duration_opacity_in = 0.3;
 
-		// Set initial styles for hover chars
-		gsap.set(hoverSplit.chars, {
+		const stagger_out = 0.05;
+		const stagger_in = 0.05;
+
+		const delay = 0.05;
+
+		const opacityStartOffset = duration_in - duration_opacity_in;
+
+		const fan_out = 0.1;
+		const fan_in = 0.1;
+
+		const rotation_out = 1;
+		const rotation_in = 1;
+
+		const y_out = 20;
+		const y_in = 20;
+
+		const ease_out = "power2.inOut";
+		const ease_in = "power2.inOut";
+		const ease_opacity_out = "power2.inOut";
+		const ease_opacity_in = "power2.inOut";
+
+		const scale_out = 0.95;
+		const scale_in = 0.95;
+
+		// Find elements
+		const text_el_out = button.querySelector(".button_text.is-default");
+		const text_el_in = button.querySelector(".button_text.is-hover");
+		const bg = button.querySelector(".button_bg");
+
+		let els_out, els_in; // animated elements
+		let split_out, split_in; // split text objs
+
+		// if we want to animate words
+		if (splitType === "words") {
+			split_out = new SplitText(text_el_out, {
+				type: "chars, words",
+				wordsClass: el_class_out,
+			});
+			split_in = new SplitText(text_el_in, {
+				type: "chars, words",
+				wordsClass: el_class_in,
+			});
+			els_out = split_out.words;
+			els_in = split_in.words;
+		}
+		// if we want to animate chars
+		else {
+			split_out = new SplitText(text_el_out, {
+				type: "chars, words",
+				charsClass: el_class_out,
+			});
+			split_in = new SplitText(text_el_in, {
+				type: "chars, words",
+				charsClass: el_class_in,
+			});
+			els_out = split_out.chars;
+			els_in = split_in.chars;
+		}
+
+		const center_out = (els_out.length - 1) / 2;
+		const center_in = (els_in.length - 1) / 2;
+
+		// 4. Initial state
+		gsap.set(els_in, {
 			opacity: 0,
-			x: (i) => (i - centerHover) * fan,
-			y: 20,
-			rotation: 5,
-			scale: 0.95,
+			x: (i) => (i - center_in) * fan_in,
+			y: y_in,
+			rotation: (i) => (i - center_in) * rotation_in,
+			scale: scale_in,
 		});
 
-		gsap.set(hoverTextEl, { opacity: 1 });
+		gsap.set(text_el_in, { opacity: 1 });
 
-		let tl = gsap.timeline({ paused: true, defaults: { duration: 0.15 } });
+		// 5. Timeline
+		let tl = gsap.timeline({ paused: true, defaults: { duration: 0.15, ease: "power2.inOut" } });
 
 		tl.to(
-			defaultSplit.chars,
+			els_out,
 			{
-				y: -20,
-				x: (i) => (i - center) * fan,
-				rotation: (i) => (i - center) * rotation,
-				scale: 0.95,
-				ease: ease,
+				y: -y_out,
+				x: (i) => (i - center_out) * fan_out,
+				rotation: (i) => (i - center_out) * rotation_out,
+				scale: scale_out,
+				ease: ease_out,
+				duration: duration_out,
 				stagger: {
-					each: stagger,
+					each: stagger_out,
 					from: "start",
 				},
 			},
 			"0"
 		);
 		tl.to(
-			defaultSplit.chars,
+			els_out,
 			{
 				opacity: 0,
-				duration: 0.1,
+				duration: duration_opacity_out,
+				ease: ease_opacity_out,
 				stagger: {
-					each: stagger,
+					each: stagger_out,
 					from: "start",
 				},
 			},
 			"0"
 		);
 		tl.to(
-			hoverSplit.chars,
+			els_in,
 			{
 				y: 0,
 				x: 0,
 				rotation: 0,
 				scale: 1,
-				ease: easeHover,
+				ease: ease_in,
+				duration: duration_in,
+
 				stagger: {
-					each: staggerHover,
+					each: stagger_in,
 					from: "start",
 				},
 			},
-			hoverDelay
+			delay
 		);
 		tl.to(
-			hoverSplit.chars,
+			els_in,
 			{
 				opacity: 1,
-				duration: 0.1,
-				ease: easeHover,
+				duration: duration_opacity_in,
+				ease: ease_opacity_in,
 				stagger: {
-					each: staggerHover,
+					each: stagger_in,
 					from: "start",
 				},
 			},
-			hoverDelay
+			delay + opacityStartOffset
+		);
+		tl.to(
+			bg,
+			{
+				scale: 1.05,
+				duration: 0.3,
+				ease: "power2.inOut",
+			},
+			0
 		);
 
 		tl.timeScale(1);
 
+		// 6. Triggers
 		button.addEventListener("mouseenter", () => {
 			tl.play();
 		});
