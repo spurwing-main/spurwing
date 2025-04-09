411ff188b2897e798f6ced9238efacea9321a1fe
sampilgrim-sw <sam.pilgrim@spurwing.co.uk>
2025-04-09 11:41:44 +0100

    remove scroll events on 404

--- spurwing-2025/spurwing.js
+++ spurwing-2025/spurwing.js
@@ -962,6 +962,12 @@ function main() {
 					render: { visible: false },
 				},
 			});
+
+			// Disable scroll capturing
+			mouse.element.removeEventListener("wheel", mouse.mousewheel);
+			mouse.element.removeEventListener("mousewheel", mouse.mousewheel);
+			mouse.element.removeEventListener("DOMMouseScroll", mouse.mousewheel);
+
 			World.add(engine.world, mouseConstraint);
 			render.mouse = mouse;
 
